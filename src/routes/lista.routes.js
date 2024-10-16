6865558fa6229ace2dfb98e527443991cf220a2d
ismael <134803262+joseismaelvargas@users.noreply.github.com>
2024-10-16 15:28:06 -0300

    Update lista.routes.js

--- src/routes/lista.routes.js
+++ src/routes/lista.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { crearLista,mostrarLista,editarLista,deleteLista} from "../controllers/listacontroller.js";
 const ruta=Router();
 // ruta.route('/prueba').get(prueba)
-ruta.route('/crearlista').post(crearLista)
+// ruta.route('/crearlista').post(crearLista)
 ruta.route('/verLista').post(crearLista).get(mostrarLista)
 ruta.route('/editlista/:id').put(editarLista).delete(deleteLista)
 
